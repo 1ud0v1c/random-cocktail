b461de96dad45f565095c300664785a863099ebd
1ud0v1c <ludovic.vimont@gmail.com>
2019-08-17 18:06:18 +0200

    MainActivity layout

---
+++ app/src/main/java/com/ludovic/vimont/randomcocktail/activities/RandomActivity.kt
@@ -0,0 +1,12 @@
+package com.ludovic.vimont.randomcocktail.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ludovic.vimont.randomcocktail.R
+
+class RandomActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_random)
+    }
+}
